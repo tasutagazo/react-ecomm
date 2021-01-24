6774a6a77d72699ea4601ba79b84b80bbdc413c2
tasutagazo <balabak.tp@gmail.com>
2021-01-24 23:50:19 +0800

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,11 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reduce';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger)
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
